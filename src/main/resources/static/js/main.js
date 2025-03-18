e490882c9d8b745d69b939eb339aa811786cc9f4
Héctor Uriostegui <hectoruu@gmail.com>
2025-03-18 10:39:19 -0600

    Agregar paginación y botón modo oscuro.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -48,37 +48,3 @@ window.addEventListener("load", () => {
 
 // Asignar evento al botón
 document.querySelector("#dl-icon").addEventListener("click", changeTheme);
-
-
-/*const darkTheme = () => {
-    const body = document.querySelector("body");
-    body.setAttribute("data-bs-theme", "dark");
-    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
-
-    // Cambiar todas las clases bg-primary a bg-body-secondary
-    document.querySelectorAll(".bg-primary").forEach((elemento) => {
-        elemento.classList.remove("bg-primary");
-        elemento.classList.add("bg-body-secondary");
-    });
-    localStorage.setItem("theme", "dark");
-};
-
-const lightTheme = () => {
-    const body = document.querySelector("body");
-    body.setAttribute("data-bs-theme", "light");
-    document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
-
-    // Cambiar todas las clases bg-body-secondary a bg-primary
-    document.querySelectorAll(".bg-body-secondary").forEach((elemento) => {
-        elemento.classList.remove("bg-body-secondary");
-        elemento.classList.add("bg-primary");
-    });
-    localStorage.setItem("theme", "light");
-};
-
-const changeTheme = () => {
-    const body = document.querySelector("body");
-    body.getAttribute("data-bs-theme") === "light" ? darkTheme() : lightTheme();
-};*/
-
-
